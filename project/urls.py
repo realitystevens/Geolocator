da24b4cf74e80ce62b11bce4e1b042d9687edb45
Stevens Reality <stevensreality@outlook.com>
2023-10-22 20:54:07 +0100

    add staticfile settings

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('backend/', admin.site.urls),
-     path('', include('app.urls')),
-]
+    path('', include('app.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
