5efed5fd01edec61afe634a47f6981c45147593b
Stevens Reality <stevensreality@outlook.com>
2023-10-22 21:09:17 +0100

    changed staticfiles_dirs

--- project/settings.py
+++ project/settings.py
@@ -89,10 +89,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     '/var/www/static/',
+# ]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
