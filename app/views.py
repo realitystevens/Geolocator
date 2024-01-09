e50e52f90c98d4c29ff2eaa2bd71bebb498ea026
Stevens Reality <stevensreality@outlook.com>
2024-01-09 23:46:43 +0100

    Worked on webapp logic

--- app/views.py
+++ app/views.py
@@ -1,28 +1,29 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
-from json import JSONDecodeError
-from django.http import JsonResponse
-from django.http import HttpResponse
+from django.conf import settings
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+
 
 load_dotenv()
 
 
 def index(request):
-
     if request.method == 'POST':
         query = request.POST.get('query')
-        BING_MAPS_KEY = os.environ.get('BING_MAPS_KEY')
+        BINGMAPS_APIKEY = settings.BINGMAPS_APIKEY
 
-        url = f'https://dev.virtualearth.net/REST/v1/Locations?query={query}&key={BING_MAPS_KEY}&maxResults=1'
+        url = f'https://dev.virtualearth.net/REST/v1/Locations?query={query}&key={BINGMAPS_APIKEY}&maxResults=1'
 
         response = requests.get(url)
 
         try:
             data = response.json()
-        except JSONDecodeError:
-            data = {'error': 'The API response is not in JSON format'}
+        except Exception as e:
+            data = {'error': str(e)}
 
         context = {
             'data': data,
