5c5cb8eafa8419bd1633c8d675d5a8b6d9460ac3
Stevens Reality <stevensreality@outlook.com>
2025-01-28 14:35:54 +0100

    ci: configure build commands for deployment

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # exit on error
 set -o errexit
 
+echo "Building project..."
+echo "Installing dependencies..."
 pip install --upgrade pip
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
+echo "Finished Building project"
