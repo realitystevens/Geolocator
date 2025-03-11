be62398d2702173c9919de901ac14088b5315370
Stevens Reality <stevensreality@outlook.com>
2025-03-11 21:59:24 +0100

    update: revamp functionality to suit functionality coming from 'static/getCurrentLocation.js'

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from forms import GeolocatorForm
 from flask_wtf import CSRFProtect
 from flask import Flask, render_template
 from dotenv import load_dotenv
+from flask import request, jsonify
+import json
 
 load_dotenv()
 
@@ -19,26 +21,32 @@ csrf = CSRFProtect(app)
 def index():
     form = GeolocatorForm()
     if form.validate_on_submit():
-        params = {
-            "q": form.address.data,
-            "format": "json",
-            "addressdetails": 1
-        }
-
         headers = {
             "User-Agent": "Geolocator/1.0 (reality.stvns@gmail.com)"
         }
 
-        url = "https://nominatim.openstreetmap.org/search"
-
-        response = requests.get(url, params=params, headers=headers)
+        if form.data['address'].startswith('{lat:'):
+            address_data = json.loads(form.data['address'])
+            params = {
+                "lat": address_data['lat'],
+                "lon": address_data['lon'],
+                "format": "json",
+                "addressdetails": 1
+            }
+            url = "https://nominatim.openstreetmap.org/reverse"
+        else:
+            params = {
+                "q": form.data['address'],
+                "format": "json",
+                "addressdetails": 1
+            }
+            url = "https://nominatim.openstreetmap.org/search"
 
         try:
-            if response.status_code == 200:
-                data = response.json()
-                return render_template('index.html', form=form, data=data)
-            else:
-                response.raise_for_status()
+            response = requests.get(url, params=params, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+            return render_template('index.html', form=form, data=data)
         except requests.exceptions.RequestException as e:
             error = f"Error fetching data from the API: {e}"
             return render_template('index.html', form=form, error=error)
