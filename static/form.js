48cdaf8ac6c01d600885bbe62cf30f40431ee028
Stevens Reality <stevensreality@outlook.com>
2025-03-11 21:58:24 +0100

    refactor: revamp and optmized codebase

--- static/form.js
+++ static/form.js
@@ -1,12 +1,17 @@
-(function() {
-    const inputText = document.getElementById('input_text');
-    const actionBtn = document.getElementById('action_btn');
+const inputText = document.getElementById('input_text');
+const actionBtn = document.getElementById('action_btn');
 
-    inputText.addEventListener("input", function() {
-        actionBtn.disabled = false; 
 
-        if (inputText.value === '') {
-            actionBtn.disabled = true; 
-        }
-    });
-})();
+
+```
+Function to enable the action button when the input field is not empty
+```
+inputText.addEventListener("input", function() {
+    actionBtn.disabled = false; 
+
+    if (inputText.value === '') {
+        actionBtn.disabled = true; 
+    }
+});
+
+export { actionBtn, inputText };
