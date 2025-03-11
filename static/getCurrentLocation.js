0b282d56b9bcbea26ae56c98517dd091fb3293fb
Stevens Reality <stevensreality@outlook.com>
2025-03-11 21:51:45 +0100

    refactor: revamp logic on using the 'Use current location' button

--- static/getCurrentLocation.js
+++ static/getCurrentLocation.js
@@ -1,51 +1,36 @@
-(function() {
-    const currentLocation = document.getElementById('current_location');
-    const addressGetLocation = document.querySelector('.address_getLocation');
+import { inputText, actionBtn } from "./form.js";
 
-    const getCurrentLocation = () => {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                position => {
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-            
-                    const locationData = {
-                        latitude: latitude,
-                        longitude: longitude
-                    };
-            
-                    console.log(locationData);
-                    
-                    // Send the latitude and longitude to the Flask app
-                    fetch('http://localhost:5000/get_location', {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(locationData)
-                    })
-                    .then(response => {
-                        if (!response.ok) {
-                          throw new Error(`HTTP error! status: ${response.status}`);
-                        }
-                        return response.json()
-                      })
-                      .then(data => {
-                        console.log('Success:', data);
-                      })
-                      .catch(error => {
-                        console.error('Error:', error);
-                      });
-                },
-                error => {
-                    console.error(error);
-                }
-            );
-        } else {
-            console.error("Geolocation is not supported by this browser.");
-        }
-    };
 
-    // Make getCurrentLocation() available in the global scope
-    window.getCurrentLocation = getCurrentLocation;
-})();
+
+```
+Function to get the current location of the user
+```
+const getCurrentLocation = () => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+
+                const locationData = {
+                    lat: latitude,
+                    lon: longitude,
+                };
+
+                inputText.value = JSON.stringify(locationData);
+                actionBtn.disabled = false;
+            },
+            error => {
+                console.error(error);
+            }
+        );
+    } else {
+        console.error("Geolocation (in Navigator) is not supported by this browser.");
+    }
+};
+
+
+```
+Make function globally accessible
+```
+window.getCurrentLocation = getCurrentLocation;
